01b0e549885a1b3d4c7c6ed061ebebe91fcde5c0
Victor Henrique <115226771+Tipovito@users.noreply.github.com>
2022-11-16 09:13:38 -0300

    imbapp
    Criação da Dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,14 @@
 @extends('template')
 
 @section('conteudo')
-colocar conteudo aqui DASHBOARD
+<div class="container mt-5 p-5 bg-verde">
+    <div class="row">
+        <div class="col">
+            <input type="text" class="form-control" placeholder="First name" aria-label="First name">
+        </div>
+        <div class="col">
+            <input type="text" class="form-control" placeholder="Last name" aria-label="Last name">
+        </div>
+    </div>
+</div>
 @endsection
--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -10,13 +10,34 @@
   <!-- Bootstrap CSS v5.2.1 -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-
+    
+    <link rel="stylesheet" href="{{url('/')}}/style.css">
 </head>
 
 <body>
   <header>
     <!-- place navbar here -->
-    Barra de navegação aqui
+    <nav class="navbar navbar-expand-sm navbar-light bg-verde">
+        <div class="container">
+        <a class="navbar-brand" href="#">IMB</a>
+        <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+          aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="collapsibleNavId">
+          <ul class="navbar-nav ms-auto mt-2 mt-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" href="#" aria-current="page">Home <span class="visually-hidden">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Sair</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    
+
   </header>
   <main>
       @yield('conteudo')
